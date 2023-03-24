8c4e5e5aea545c569b5a23fd6a5f6a8b9e9792a3
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-03-24 15:13:22 +0100

    esercizio base completato

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    data(){
+        return {
+            message: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+
+<template>
+
+    <div>
+        <h1 class="title">
+            {{ message }}
+        </h1>
+    </div>
+
+</template>
+
+<style scoped>
+.title {
+    color: blueviolet;
+    border: 2px solid blue;
+    padding: 30px;
+}
+</style>
